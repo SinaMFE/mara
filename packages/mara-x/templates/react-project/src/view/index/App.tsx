a81e03055dbb67810ceef6730c3c153356c57d51
Vincent Lee <vv314@foxmail.com>
2018-12-24 14:55:12 +0800

    chore: React template using typescript

--- packages/mara-x/templates/react-project/src/view/index/App.tsx
+++ packages/mara-x/templates/react-project/src/view/index/App.tsx
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <Logo />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
---
+++ packages/mara-x/templates/react-project/src/view/index/shims-react.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="@mara/x" />
