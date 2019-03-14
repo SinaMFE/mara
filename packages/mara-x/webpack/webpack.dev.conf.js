dd6a14dce7888508ee99ccec7b4f54f22ac62731
Vincent Lee <vv314@foxmail.com>
2019-03-14 23:53:23 +0800

    chore: splitChunks uses async by default

--- packages/mara-x/webpack/webpack.dev.conf.js
+++ packages/mara-x/webpack/webpack.dev.conf.js
@@ -47,7 +47,7 @@ module.exports = function({ entry, spinner }) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        chunks: 'all',
+        chunks: config.compiler.splitChunks ? 'all' : 'async',
         name: false
       },
       // Keep the runtime chunk seperated to enable long term caching
